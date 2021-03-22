d9148f9d95e9d1de75f28d5185582bea5aed4358
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-03-22 19:14:10 +0300

    Defines calculate method

--- vazhin/index.js
+++ vazhin/index.js
@@ -1,13 +1,24 @@
 class Calculator {
   constructor() {
     this.lastType = null;
-    this.lastOperator = null;
     this.newExpression = null;
     this.inputQueue = [];
     this.precedenceScore = { '×': 2, '÷': 2, '+': 1, '-': 1 };
+    this.operations = {
+      '+': (num1, num2) => num1 + num2,
+      '-': (num1, num2) => num1 - num2,
+      '×': (num1, num2) => num1 * num2,
+      '÷': (num1, num2) => num1 / num2,
+    };
   }
 
-  calculate() {}
+  calculate(inputQueue) {
+    const result = this.calculateExpression(inputQueue[0]);
+    inputQueue.shift();
+    if (this.isEmpty(inputQueue)) return result;
+    inputQueue[0].leftNum = result;
+    return this.calculate(inputQueue);
+  }
 
   addExpression() {
     if (!this.newExpression || !this.isValidExpression(this.newExpression))
@@ -38,4 +49,37 @@ class Calculator {
       expression.leftNum && expression.operator && expression.rightNum;
     return isValid;
   }
+
+  // Utilities
+
+  calculateExpression(expression) {
+    return this.operations[expression.operator](
+      expression.leftNum,
+      expression.rightNum
+    );
+  }
+
+  isEmpty(arr) {
+    return arr.length === 0;
+  }
 }
+
+const calculator = new Calculator();
+
+calculator.newExpression = {
+  operator: '+',
+  leftNum: 2,
+  rightNum: 3.5,
+};
+calculator.addExpression();
+
+calculator.newExpression = {
+  operator: '×',
+  leftNum: 3.5,
+  rightNum: 3.5,
+};
+calculator.addExpression();
+
+console.log(calculator.inputQueue);
+
+console.log(calculator.calculate(calculator.inputQueue));
