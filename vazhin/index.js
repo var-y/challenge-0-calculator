5a36067b069276e859cf991db7a306e7b11cb963
Vazhin Tayeb <vazhintayeb@gmail.com>
2021-03-22 18:04:43 +0300

    Fixes compareExpressions method & cleans up code

--- vazhin/index.js
+++ vazhin/index.js
@@ -1,45 +1,29 @@
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('button');
-
 class Calculator {
   constructor() {
     this.lastType = null;
     this.lastOperator = null;
     this.newExpression = null;
     this.inputQueue = [];
-    // this.insert = {
-    //   FIRST: (newExpression) => this.inputQueue.unshift(newExpression),
-    //   LAST: (newExpression) => this.inputQueue.push(newExpression),
-    // };
     this.precedenceScore = { '×': 2, '÷': 2, '+': 1, '-': 1 };
   }
 
   calculate() {}
 
   addExpression() {
-    if (!this.newExpression || !isValidExpression(this.newExpression)) return;
-    // const insertEnd = this.getInsertEnd(
-    //   this.newExpression.operator,
-    //   this.lastOperator
-    // );
-
-    // this.insert[insertEnd];
+    if (!this.newExpression || !this.isValidExpression(this.newExpression))
+      return;
 
     this.inputQueue.push(this.newExpression);
-    this.inputQueue.sort(this.compareExpressions);
-  }
 
-  // Got rid of getInsertEnd method because we might want to insert in the middle of inputQueue not just the ends, based on operation order
-  // getInsertEnd(newOperator, lastOperator) {
-  //   const resultOfComparison = compareExpressions(newOperator, lastOperator);
-  //   if ([0, -1].includes(resultOfComparison)) return '';
-  // }
+    if (this.inputQueue.length > 1)
+      this.inputQueue.sort(this.compareExpressions.bind(this));
+  }
 
   compareExpressions(A, B) {
     const precedenceOfA = this.precedenceScore[A.operator];
     const precedenceOfB = this.precedenceScore[B.operator];
-    if (precedenceOfA > precedenceOfB) return +1;
-    if (precedenceOfA < precedenceOfB) return -1;
+    if (precedenceOfA > precedenceOfB) return -1;
+    if (precedenceOfA < precedenceOfB) return +1;
     return 0;
   }
 
